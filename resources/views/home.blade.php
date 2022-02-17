f0a01be8a9f76e80411ba7ef07d66e2f3da8b6d6
Maccio92 <valerio362@gmail.com>
2022-02-17 15:20:19 +0100

    add bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,16 @@
 @extends('layout.base')
 
 @section('content')
+<div class="container">
     @foreach ($movies as $movie)
         <div class="card">
             <h2>{{$movie -> id}}</h2>
-            <h2>{{$movie -> title}}</h2>
-            <h3>{{$movie -> original_title}}</h3>
-            <h3>{{$movie -> nationality}}</h3>
-            <h4>{{$movie -> date}}</h4>
-            <h4>{{$movie -> vote}}</h4>
+            <h2>Titolo: {{$movie -> title}}</h2>
+            <h3>Titolo Originale: {{$movie -> original_title}}</h3>
+            <h3>Nazionalità: {{$movie -> nationality}}</h3>
+            <h4>Data di uscita: {{$movie -> date}}</h4>
+            <h4>Voto: {{$movie -> vote}}</h4>
         </div>
     @endforeach
+</div>
 @endsection
